989e3f8739900f4df4fdfdec4b0d048526c43c5f
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-02-20 19:51:02 -0600

    table of contents

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,4 +1,33 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const userInput = ({}) =>
+`
+# Title
+
+## Table of Contents
+-[Description]
+-[Installation]
+-[Usage]
+-[Credits]
+-[License]
+-[Test]
+-[Questions?]
+
+## Description 
+
+## Installation 
+
+## Usage
+
+## Credits 
+
+## License 
+
+## Test
+
+## Questions? 
+`
 
 // TODO: Create an array of questions for user input
 const questions = [];
